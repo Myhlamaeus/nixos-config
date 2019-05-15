92c4ba46bb8802f6f58e2e7fa7410aa18caa87bc
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2019-05-15 18:33:44 +0000

    feat(system): enable automatic updates

--- system/configuration.nix
+++ system/configuration.nix
@@ -209,4 +209,8 @@ in
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "19.03"; # Did you read the comment?
+  system.autoUpgrade = {
+    enable = true;
+    dates = "05:00";
+  };
 }
