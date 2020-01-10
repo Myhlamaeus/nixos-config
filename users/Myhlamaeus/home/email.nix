9920ddc1d6b676127c06fc85b0baf9e9f48444c3
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-01-10 06:52:02 +0000

    fix(myhlamaeus): fix gmail password location

--- users/Myhlamaeus/home/email.nix
+++ users/Myhlamaeus/home/email.nix
@@ -23,7 +23,7 @@
       msmtp = {
         enable = true;
       };
-      passwordCommand = "${pkgs.pass}/bin/pass show 'Private Passwords/Email/Google'";
+      passwordCommand = "${pkgs.pass}/bin/pass show private/emails/gmail";
       gpg = {
         key = "7FCB362E2D975AD2A45A682CAD1390B6FE33C758";
       };
