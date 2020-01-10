59cdec1b0195cd499e99e9bb85e8cadb6fc7fb7f
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-01-10 06:44:04 +0000

    feat(system): enable all the firmware

--- system/configuration.nix
+++ system/configuration.nix
@@ -67,6 +67,8 @@ in
       ./users
     ];
 
+  hardware.enableAllFirmware = true;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
