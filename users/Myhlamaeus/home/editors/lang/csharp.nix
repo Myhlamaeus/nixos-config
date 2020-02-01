d62973d88e68a647bd3c8166897aa8afe8cd694d
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-02-01 13:47:05 +0000

    feat(myhlamaeus): add csharp language server
    Add the csharp language server globally as it would add too much
    overhead if added per-project.

---
+++ users/Myhlamaeus/home/editors/lang/csharp.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+with pkgs;
+let
+  pkgs-unstable = (import <nixpkgs-unstable> { config = { allowUnfree = true; }; });
+  dotnet-combined = with pkgs-unstable.dotnetCorePackages; combinePackages [ sdk_3_1 sdk_2_1 ];
+  dotnetRoot = "${dotnet-combined}/bin";
+  dotnetSdk = "${dotnet-combined}/sdk";
+  dotnetBinary = "${dotnetRoot}/dotnet";
+in
+{
+  nixpkgs.overlays = [
+    (self: super: {
+      omnisharp-roslyn = super.omnisharp-roslyn.overrideAttrs (oldAttrs: rec {
+          version = "1.34.9";
+          src = fetchurl {
+            url = "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v${version}/omnisharp-mono.tar.gz";
+            sha256 = "1b5jzc7dj9hhddrr73hhpq95h8vabkd6xac1bwq05lb24m0jsrp9";
+          };
+      });
+    })
+  ];
+
+  home.packages = [
+    # dotnet-combined
+    omnisharp-roslyn
+  ];
+
+  home.sessionVariables = {
+    DOTNET_ROOT = dotnetRoot;
+    MSBuildSdksPath = "${dotnetSdk}/$(${dotnetBinary} --version)/Sdks";
+    MSBUILD_EXE_PATH = "${dotnetSdk}/$(${dotnetBinary} --version)/MSBuild.dll";
+  };
+
+  home.file.".omnisharp/omnisharp.json" = {
+    text = builtins.toJSON {
+      RoslynExtensionsOptions = {
+        EnableAnalyzersSupport = true;
+      };
+    };
+  };
+}
