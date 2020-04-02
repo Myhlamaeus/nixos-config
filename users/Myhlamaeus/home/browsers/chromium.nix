aa233aa8f5b629fc896bb86e08df2f4ce870af5f
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-04-02 19:14:32 +0000

    fix(myhlamaeus): force dark mode in Chromium

--- users/Myhlamaeus/home/browsers/chromium.nix
+++ users/Myhlamaeus/home/browsers/chromium.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (self: super: {
+      chromium = super.chromium.override {
+        commandLineArgs = "--force-dark-mode";
+      };
+    })
+  ];
+
   programs.chromium = {
     enable = true;
     extensions = [
