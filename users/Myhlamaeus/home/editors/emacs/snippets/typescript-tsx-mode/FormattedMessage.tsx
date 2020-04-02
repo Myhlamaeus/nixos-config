2c08100a99a493196ee2e9d427c270bac52925c8
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-04-02 20:39:38 +0000

    fix(myhlamaeus): fix FormattedMessage templates
    Remove the trailing comma from the FormattedMessage templates.

--- users/Myhlamaeus/home/editors/emacs/snippets/typescript-tsx-mode/FormattedMessage.tsx
+++ users/Myhlamaeus/home/editors/emacs/snippets/typescript-tsx-mode/FormattedMessage.tsx
@@ -6,4 +6,4 @@
 <FormattedMessage
   id="`(string-inflection-camelcase-function (file-name-base (directory-file-name (file-name-directory (directory-file-name (file-name-directory (directory-file-name (file-name-directory (directory-file-name (file-name-directory (buffer-file-name)))))))))))`.`(file-name-base (directory-file-name (file-name-directory (directory-file-name (file-name-directory (buffer-file-name))))))`.`(file-name-base)`.$1"
   defaultMessage="$2"
-/>,
+/>
--- users/Myhlamaeus/home/editors/emacs/snippets/typescript-tsx-mode/FormattedMessageV.tsx
+++ users/Myhlamaeus/home/editors/emacs/snippets/typescript-tsx-mode/FormattedMessageV.tsx
@@ -7,4 +7,4 @@
   id="`(string-inflection-camelcase-function (file-name-base (directory-file-name (file-name-directory (directory-file-name (file-name-directory (directory-file-name (file-name-directory (directory-file-name (file-name-directory (buffer-file-name)))))))))))`.`(file-name-base (directory-file-name (file-name-directory (directory-file-name (file-name-directory (buffer-file-name))))))`.`(file-name-base)`.$1"
   defaultMessage="$2"
   values={{ $3 }}
-/>,
+/>
