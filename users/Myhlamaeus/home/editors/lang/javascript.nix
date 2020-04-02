0109dc8fd7085e098f8360d51bfdb4e47419f75d
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-04-02 20:05:36 +0000

    fix(myhlamaeus): add global npm deps to emacs
    Add global npm deps to emacs as some language servers aren't yet available via nix.

--- users/Myhlamaeus/home/editors/lang/javascript.nix
+++ users/Myhlamaeus/home/editors/lang/javascript.nix
@@ -3,5 +3,9 @@
 {
   # Doesn't work because bin/ is a symlink
   # custom.editors.env.bin.packages = with pkgs.nodePackages; [ eslint prettier typescript-language-server ];
-  home.packages = with pkgs.nodePackages; [ eslint prettier typescript-language-server ];
+  home.packages = with pkgs.nodePackages; [ pkgs.nodejs eslint prettier typescript-language-server ];
+  custom.editors.emacs.setup = ''
+    (setenv "PATH" (concat "/home/Myhlamaeus/.local/share/npm/bin:" (getenv "PATH")))
+    (add-to-list 'exec-path "/home/Myhlamaeus/.local/share/npm/bin" t)
+  '';
 }
