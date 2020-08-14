663c95125d562b88189d3df2fb544339b19d31ad
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-08-14 17:16:36 +0000

    fix(myhlamaeus): remove global all-hies
    all-hies is now supposed to be used locally, per-project.

--- users/Myhlamaeus/home/editors/lang/haskell.nix
+++ users/Myhlamaeus/home/editors/lang/haskell.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 
-let
-  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-in
 {
   custom.editors.env.bin.packages = [
-    (all-hies.unstable.selection {
-      selector = p: { inherit (p) ghc864 ghc865 ghc881 ghc882; };
-    })
   ];
 }
