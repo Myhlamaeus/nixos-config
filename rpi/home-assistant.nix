1f4d9515372b9bdb4fa76f0ea45546e8c6f79d48
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-10-21 12:01:50 +0000

    feat(rpi): add bedroom light automation

--- rpi/home-assistant.nix
+++ rpi/home-assistant.nix
@@ -68,6 +68,196 @@ in
           port = config.local.services.deconz.httpPort;
           api_key = "!secret deconzSecret";
         };
+
+        sensor = [
+          {
+            platform = "time_date";
+            display_options = [ "time" ];
+          }
+        ];
+
+        scene = [
+          {
+            name = "Wake-up";
+            entities = {
+              "light.bedroom" = {
+                state = "on";
+                transition = 600;
+                brightness_pct = 100;
+                kelvin = 4000;
+              };
+            };
+          }
+
+          {
+            name = "Away bedroom";
+            entities = {
+              "light.bedroom" = {
+                state = "off";
+              };
+            };
+          }
+
+          {
+            name = "Day bedroom";
+            entities = {
+              "light.bedroom" = {
+                state = "off";
+              };
+            };
+          }
+
+          {
+            name = "Evening bedroom";
+            entities = {
+              "light.bedroom" = {
+                state = "on";
+                transition = 600;
+                brightness_pct = 80;
+                kelvin = 3000;
+              };
+            };
+          }
+
+          {
+            name = "Night bedroom";
+            entities = {
+              "light.bedroom" = {
+                state = "on";
+                transition = 600;
+                brightness_pct = 40;
+                kelvin = 2000;
+              };
+            };
+          }
+        ];
+
+        automation = [
+          {
+            alias = "Wake-up";
+
+            trigger = {
+              platform = "time";
+              at = "04:00:00";
+            };
+
+            condition = [
+              {
+                condition = "state";
+                entity_id = "person.maurice";
+                state = "home";
+              }
+            ];
+
+            action = [
+              {
+                service = "scene.turn_on";
+                entity_id = "scene.wake_up";
+              }
+            ];
+          }
+
+          {
+            alias = "Day Bedroom";
+
+            trigger = {
+              platform = "time";
+              at = "07:00:00";
+            };
+
+            action = {
+              service = "scene.turn_on";
+              entity_id = "scene.day_bedroom";
+            };
+          }
+
+          {
+            alias = "Evening Bedroom";
+
+            trigger = [
+              {
+                platform = "sun";
+                event = "sunset";
+                offset = "-01:00:00";
+              }
+
+              {
+                platform = "state";
+                entity_id = "person.maurice";
+                to = "home";
+              }
+            ];
+
+            condition = [
+              {
+                condition = "state";
+                entity_id = "person.maurice";
+                state = "home";
+              }
+
+              {
+                condition = "time";
+                after = "16:00:00";
+                before = "18:00:00";
+              }
+            ];
+
+            action = {
+              service = "scene.turn_on";
+              entity_id = "scene.evening_bedroom";
+            };
+          }
+
+          {
+            alias = "Night Bedroom";
+
+            trigger = [
+              {
+                platform = "time";
+                at = "18:00:00";
+              }
+
+              {
+                platform = "state";
+                entity_id = "person.maurice";
+                to = "home";
+              }
+            ];
+
+            condition = [
+              {
+                condition = "state";
+                entity_id = "person.maurice";
+                state = "home";
+              }
+
+              {
+                condition = "time";
+                after = "18:00:00";
+              }
+            ];
+
+            action = {
+              service = "scene.turn_on";
+              entity_id = "scene.night_bedroom";
+            };
+          }
+
+          {
+            alias = "Away Bedroom";
+
+            trigger = {
+              platform = "state";
+              entity_id = "person.maurice";
+              to = "not_home";
+            };
+
+            action = {
+              service = "scene.turn_on";
+              entity_id = "scene.away_bedroom";
+            };
+          }
+        ];
       };
       # configWritable = true; # doesn't work atm
     };
