7972625fb4eb50f6a20e6b003d8a421dcf6e78fe
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2020-11-22 07:15:18 +0000

    feat(myhlamaeus): re-enable chromium
    Re-enable chromium for when pages don't work in firefox.

--- users/Myhlamaeus/home/browsers/chromium.nix
+++ users/Myhlamaeus/home/browsers/chromium.nix
@@ -3,38 +3,13 @@
 with lib; {
   config = mkIf config.custom.x11.enable {
     programs.chromium = {
-      enable = false;
+      enable = true;
       extensions = [
-        "fhgenkpocbhhddlgkjnfghpjanffonno" # Authy Chrome Extension
-        # "agoopbiflnjadjfbhimhlmcbgmdgldld" # Baseliner
         "naepdomgkenhinolocfifgehidddafch" # Browserpass
-        # "jifpbeccnghkjeaalbbjmodiffmgedin" # Chrome extension source viewer
-        # "inomeogfingihgjfjlpeplalcfajhgai" # Chrome Remote Desktop
         "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-        # "okadibdjfemgnhjiembecghcbfknbfhg" # Enhanced Steam
-        # "pioclpoplcdbaefihamjohnefbikjilc" # Evernote Web Clipper
-        "hoccpcefjcgnabbmojbfoflggkecmpgd" # github-vscode-icons
-        "ghbmnnjooekpmoecnnnilnnbdlolhkhi" # Google Docs Offline
-        "kbfnbcaeplbcioakkpcpgfkobkghlhen" # Grammarly for Chrome
         "gcbommkclmclpchllfjekcdonpmejbdp" # HTTPS Everywhere
-        "bjfhmglciegochdpefhhlphglcehbmek" # Hypothesis - Web & PDF Annotation
-        "gbmdgpbipfallnflgajpaliibnhdgobh" # JSON Viewer
-        # "flcpelgcagfhfoegekianiofphddckof" # KB SSL Enforcer
-        # "mahhgdkliaknjffpmocpaglcoljnhodn" # Memrise Button
-        "lmjdlojahmbbcodnpecnjnmlddbkjhnn" # Notifier for GitHub
-        "kkkjlfejijcjgjllecmnejhogpbcigdc" # Org Capture
-        "chlffgpmiacpedhhbkiomidkjlcfhogd" # Pushbullet
-        # "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
-        "kbmfpngjjgdllneeigpgjifpgocmfgmb" # Reddit Enhancement Suite
-        # "lmhkpmbekcpmknklioeibfkpmmfibljd" # Redux DevTools
-        "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
-        # "clngdbkpkpeebahjckkjfobafhncgmne" # Stylus
-        # "nlnkcinjjeoojlhdiedbbolilahmnldj" # Tab Sorter
-        "eggkanocgddhmamlbiijnphhppkpkmkl" # Tabs Outliner
-        "dhdgffkkebhmkfjojejmpbldmpobfkfo" # Tampermonkey
         "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
         "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-        "bhmmomiinigofkjcapegjjndpbikblnp" # WOT Web of Trust, Website Reputation Ratings
       ];
     };
     programs.browserpass.browsers = [ "chromium" ];
