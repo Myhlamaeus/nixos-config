af6b2ee6abbb900b646be4f5564d4d827c07e029
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2021-08-22 20:01:16 +0000

    feat(myhlamaeus): use passwordFile

--- users/Myhlamaeus/default.nix
+++ users/Myhlamaeus/default.nix
@@ -8,6 +8,7 @@
     shell = pkgs.zsh;
     home = "/home/Myhlamaeus";
     extraGroups = [ "wheel" "docker" "audio" "jackaudio" ];
+    passwordFile = "/etc/nixos/secrets/Myhlamaeus-password";
   };
 
   home-manager.users.Myhlamaeus = import ./home;
