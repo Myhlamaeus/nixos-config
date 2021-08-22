95b0a861814e2a1ac50e59d3abef754a98e4a209
Maurice B. Lewis <dreyer.maltem+dev@gmail.com>
2021-08-22 20:16:49 +0000

    feat(system): enable flatpak for telegram

--- system/default.nix
+++ system/default.nix
@@ -336,6 +336,10 @@
 
   services.gnome.at-spi2-core.enable = true;
 
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
